3ffc58e0f04948e52a7428ae844e4ed6494b2b85
上原祐治 <uehara@ueharayuujinoMacBook-Pro.local>
2020-05-05 20:49:38 +0900

    refactor-event-reducer

---
+++ .history/src/reducers/events_20200505202144.js
@@ -0,0 +1,29 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京オリンピックを開催します！つきましては、、、、'
+//}
+
+import {
+    CREATE_EVENT,
+    DELETE_EVENT,
+    DELETE_ALL_EVENTS
+} from '../actions'
+
+const events = (state = [], action) => {
+    switch(action.type){
+        case CREATE_EVENT:
+            const event = {title: action.title, body: action.body}
+            const length = state.length
+            const id = length === 0 ? 1 : state[length -1].id + 1
+            return [...state, {id, ...event}]
+        case DELETE_EVENT:
+            return state.filter(event => event.id !== action.id)
+        case DELETE_ALL_EVENTS:
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
